403651488b42856b25e416201a38b869d1a29d94
MatthewLoiselle <>
2021-04-28 15:50:40 +0000

    errors

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 document.getElementById('firstbutton').addEventListener('click', DisplayMessage)
 
-function DisplayMessage () {
-  alert('What did I just tell you?')
-}
+function DisplayMessage () {alert('What did I just tell you?')}
