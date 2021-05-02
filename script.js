7c48b9b2a3e55804b24655d4546b7b3ee8e6dab4
MatthewLoiselle <>
2021-05-02 06:05:16 +0000

    part 2

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.getElementById('firstbutton').addEventListener('click', DisplayMessage)
 
-function DisplayMessage () {alert('What did I just tell you?')}
+function DisplayMessage () {
+  alert('What did I just tell you?')
+}
+
+document.getElementById('button2').addEventListener('click', changetext)
+
+function changetext () {
+  document.getElementById('text').style.fontSize = '300%'
+  document.getElementById('text').style.fontFamily = 'Arial, Helvetica, sans-serif'
+  document.getElementById('text').style.backgroundColor = 'red'
+}
